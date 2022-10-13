52cdf6b640fd7265cfb1b563a0c3cc9a19f33054
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2022-10-13 19:43:49 -0300

    feat: added basic model definitions

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 (async () => {
    // Instantiate Sequelize with SQLite.
    const sequelize = new Sequelize('sqlite::memory:');
+
+   const { STRING, NUMBER } = DataTypes;
+   const User = sequelize.define('User', {
+      id: STRING,
+      name: STRING,
+      lastName: STRING,
+      age: NUMBER,
+   });
+   const Movie = sequelize.define('Movie', {
+      id: STRING,
+      title: STRING,
+      year: NUMBER,
+   });
 })();
